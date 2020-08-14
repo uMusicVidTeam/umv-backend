e9ff23f86fc5eaedd8ec7b9496643a51a1cf0ba0
Noah-Jarzab <njarzab721@gmail.com>
2020-08-14 14:21:27 -0600

    Added a Video model,
    Co-authored-by: Isaac <isaac93romano@gmail.com>

---
+++ models/Video.js
@@ -0,0 +1,15 @@
+const mongoose = require('../db/connection');
+
+const VideoSchema = new mongoose.Schema({
+    title: {type: String, required: true},
+    url: {type: String, required: true},
+    votes: {
+        upvote: {type: Number, default: 0},
+        downvote: {type: Number, default: 0}
+    },
+    genre: {type: String, required: true}
+});
+
+const Video = mongoose.model('Video', VideoSchema);
+
+module.exports = Video;
