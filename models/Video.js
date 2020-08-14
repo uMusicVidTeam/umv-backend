6a568e44baa5adcec5d2c57ce14cdb3de2be2675
Noah-Jarzab <njarzab721@gmail.com>
2020-08-14 14:54:00 -0600

    Added some json data and updated the video model,
    Co-authored-by: Isaac <isaac93romano@gmail.com>

--- models/Video.js
+++ models/Video.js
@@ -1,6 +1,7 @@
 const mongoose = require('../db/connection');
 
 const VideoSchema = new mongoose.Schema({
+    artist: {type: String, required: true},
     title: {type: String, required: true},
     url: {type: String, required: true},
     votes: {
